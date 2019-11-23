7834e69c03fcce185290e67bb417b99a8ca59d14
unknown <moyez.mansoor@gmail.com>
2019-11-23 10:48:51 -0500

    Fixed the dataset file, can read and reshape

--- dataset.py
+++ dataset.py
@@ -1,16 +1,9 @@
-from absl import app
-from absl import flags
-
 import tensorflow as tf
-import matplotlib as plt
+import numpy as np
+import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-FLAGS = flags.FLAGS
-
-flags.DEFINE_string('train', 'False', 'Path the the tf record of the train set')
-
-
-IMAGE_FEATURE_MAP = {
+image_feature_description  = {
     'image/width': tf.io.FixedLenFeature([], tf.int64),
     'image/height': tf.io.FixedLenFeature([], tf.int64),
     'image/encoded': tf.io.FixedLenFeature([], tf.string),
@@ -21,51 +14,36 @@ IMAGE_FEATURE_MAP = {
     'image/object/class/label': tf.io.VarLenFeature(tf.int64),
 }
 
-def main(argv):
-    dataset = tf.data.TFRecordDataset(FLAGS.train)
-    print(dataset)
+def _parse_image_function(example_proto):
+    return tf.io.parse_single_example(example_proto, image_feature_description)
 
-    dataset = dataset.map(_parse_function)
-    print(dataset)
-    
-    # dataset = parse_tfrecord(dataset)
+def print_img(img):
+    imgplot = plt.imshow(img.numpy().astype(dtype='uint8'))
 
-    dataset = dataset.map(parse_tfrecord)
-    print(dataset)
-    
-    img = dataset.take(1)
-
-    # img = mpimg.imread('../../doc/_static/stinkbug.png')
-    print(img)
-    
-
-
-
-
-    print('-------------')
-
-
-def _parse_function(example_proto):
-    return tf.io.parse_single_example(example_proto, IMAGE_FEATURE_MAP)
-
-def parse_tfrecord(x):
-    x_train = tf.image.decode_jpeg(x['image/encoded'], channels=3)
+def parse_tfrecord(example_proto):
+    x_train = tf.image.decode_jpeg(example_proto['image/encoded'], channels=3)
     x_train = tf.image.resize(x_train, (416, 416))
 
-    # class_text = tf.sparse.to_dense(
-    #     x['image/object/class/text'], default_value='')
-    # labels = tf.cast(class_table.lookup(class_text), tf.float32)
-    y_train = tf.stack([tf.sparse.to_dense(x['image/object/bbox/xmin']),
-                        tf.sparse.to_dense(x['image/object/bbox/ymin']),
-                        tf.sparse.to_dense(x['image/object/bbox/xmax']),
-                        tf.sparse.to_dense(x['image/object/bbox/ymax'])]
-                        , axis=1)
+    labels = tf.cast(tf.sparse.to_dense(example_proto['image/object/class/label']), tf.float32)
+    y_train = tf.stack([tf.sparse.to_dense(example_proto['image/object/bbox/xmin']),
+                        tf.sparse.to_dense(example_proto['image/object/bbox/ymin']),
+                        tf.sparse.to_dense(example_proto['image/object/bbox/xmax']),
+                        tf.sparse.to_dense(example_proto['image/object/bbox/ymax']),
+                        labels], axis=1)
 
     paddings = [[0, 100 - tf.shape(y_train)[0]], [0, 0]]
     y_train = tf.pad(y_train, paddings)
+    train = {
+        "y_train" : y_train,
+        "x_train" : x_train
+    }
 
-    return x_train, y_train
+    return train
 
+def createDataset(tfrecord_path):
+    raw_dataset = tf.data.TFRecordDataset(tfrecord_path)
+    parsed_dataset = raw_dataset.map(_parse_image_function)
+    dataset = parsed_dataset.map(parse_tfrecord)
 
-if __name__ == '__main__':
-  app.run(main)
+    return dataset
+
